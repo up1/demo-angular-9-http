521b1c2cc74b76133423fc24251bca6ce03d9091
up1 <somkiat.p@gmail.com>
2020-05-24 18:17:57 +0700

    Working with HTTP Client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DemoService } from './demo.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-app';
+
+  weatherList: any = [];
+
+  constructor(public demoService: DemoService){
+    // Get all data
+    this.demoService.GetAll().subscribe((data: {}) => {
+      this.weatherList = data;
+    });
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { DemoService } from './demo.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DemoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
